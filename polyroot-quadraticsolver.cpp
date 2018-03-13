ff14425dcf4a65ff459b292fe02668c98cb90bc3
PhantasmicSky <blake010899@gmail.com>
2018-03-13 15:04:18 +0800

    New Version

--- polyroot-quadraticsolver.cpp
+++ polyroot-quadraticsolver.cpp
@@ -1,62 +1,24 @@
 #include <cstdlib>
 #include <iostream>
-#include <cmath>
+#include <complex>
 
 using namespace std;
 
-double nroot(double a, double b, double c)
-{
-    double real=-b/(2*a);
-    if ((b*b-(4*a*c))>=0)
-    {
-        real-=((sqrt(b*b - (4*a*c)))/(2*a));
-    }
-    return real;
-}
-
-double rroot(double a, double b, double c)
-{
-    double real=-b/(2*a);
-    if ((b*b-(4*a*c))>=0)
-    {
-        real+=((sqrt(b*b - (4*a*c)))/(2*a));
-    }
-    return real;
-}
-
-
-double iroot(double a, double b, double c)
-{
-    double imag=0;
-    if ((b*b-(4*a*c))<0)
-    {
-        imag=sqrt(abs((b*b - (4*a*c))))/(2*a);
-        return imag;
-    }
-    else
-    {
-        return 0;
-    }
-}
 
 int main(int argc, char *argv[])
 {
-    double a,b,c;
-    double real, i1, i2, r2;
-    cin>> a;
+    complex<double> x[2];
+    double b,c;
     cin >> b;
     cin>>c;
-    real=rroot(a,b,c);
-    r2=nroot(a,b,c);
-    i1=iroot(a,b,c);
-    i2=-iroot(a,b,c);
-    if (i1!=0)
-    {
-     cout << "R:" <<real<<"I1:" <<i1<<"I2:"<<i2;
-    }
-    else
-    {
-        cout <<"R:" <<real<<"R:"<<r2;
-    }
+    x[0]= (b*b-4*c)/4;
+    x[0]= sqrt(x[0]);
+    x[0]= x[0];
+    x[0]+= -b/2;
+    cout << x[0];
+    x[1]= (b*b-4*c)/4;
+    x[1]= sqrt(x[1]);
+    x[1]= (-b/2)-x[1];
+    cout << x[1];
 
 }
