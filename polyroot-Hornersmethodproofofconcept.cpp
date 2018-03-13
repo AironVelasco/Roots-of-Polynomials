a26ea873ad51bb04cf06a54ba15d739284ba6df3
PhantasmicSky <blake010899@gmail.com>
2018-03-13 14:31:25 +0800

    New Version

--- polyroot-Hornersmethodproofofconcept.cpp
+++ polyroot-Hornersmethodproofofconcept.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <complex>
 
 using namespace std;
 
 int main()
 {
   int order=0;
-  double answer=0;
-  double root=0;
+  complex<double> answer=(0,0);
+  double rootr=0;
+  double rooti=0;
+  complex<double> acroot;
   double coefficient[21];
   cout <<"Highest Exponent?";
   cin>> order;
   cout <<"Root to Test?";
-  cin>> root;
+  cin>> rootr;
+  cin>>rooti;
+  acroot = {rootr,rooti}g;
+  cout <<endl<< acroot;
   for (int i=0; i<order+1;i++)
   {
     cin >> coefficient[i];
@@ -23,7 +29,7 @@ int main()
     cout <<"Before Root Answer:" <<answer<<endl;
     if (i!=order)
     {
-          answer=answer*root;
+          answer=answer*acroot;
               cout <<"After Root Answer:" <<answer<<endl;
     }
     else
